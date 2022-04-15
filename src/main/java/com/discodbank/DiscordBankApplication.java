441d77be89ecb4fe9dacbeae71de1d20797efb14
morgadope <pedron.morgdo@gmail.com>
2022-04-14 21:17:40 -0300

    firts commit

---
+++ src/main/java/com/discodbank/DiscordBankApplication.java
@@ -0,0 +1,13 @@
+package com.discodbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiscordBankApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DiscordBankApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/discodbank/DiscordBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.discodbank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscordBankApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
